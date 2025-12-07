646dfa2f115d065318a69f196e7663026944f112
maniram <maniramyadav786@gmail.com>
2025-12-07 10:33:23 +0000

    app state implmentation

--- actix_web_demo/src/main.rs
+++ actix_web_demo/src/main.rs
@@ -1,41 +1,11 @@
 use actix_web::{post,get,web, App, Responder , HttpResponse, HttpServer,Result,HttpRequest};
 use serde::Deserialize;
 use std::{
-    cell:Cell,
+    cell::Cell,
     sync::{
         Arc,
         atomic::{AtomicUsize, Ordering},
-    },
-    
-}
-
-#[derive(Deserialize)]
-struct Info {
-    username: String   ,
-}
-
-
-// Query
-#[get("/search")]
-async fn search(info : web::Query<Info>) -> String {
-    format!("Welcom: {}!", info.username)
-}
-
-
-// url encoded form data
-#[post("/submit")]
-async fn submit_form(info : web::Form<Info>) -> String {
-    format!("Welcom: {}!", info.username)
-}
-
-
-
-// Json
-#[post("/submit")]
-async fn submit(info : web::Json<Info>) -> String {
-    format!("Welcome: {}!", info.username)
-}
-
+    }, };
 
 // app state
 #[derive(Clone)]
@@ -44,15 +14,42 @@ struct AppState {
     global_count : Arc<AtomicUsize>,
 }
 
+#[get("/")]
+async fn show_count( data : web::Data<AppState>) -> impl Responder {
+    let local_count = data.local_count.get() ;
+    let global_count = data.global_count.load(Ordering::Relaxed);
+    format!("Local count: {}, Global count: {}", local_count, global_count)
+}
+
+
+#[get("/add")]
+async fn add_one( data : web::Data<AppState>) -> impl Responder {
+   
+    data.global_count.fetch_add(1, Ordering::Relaxed);
+    let local_count = data.local_count.get() ;
+    data.local_count.set(local_count+1);
+    let local_count = data.local_count.get() ;
+    let global_count = data.global_count.load(Ordering::Relaxed);
+
+    format!("Local count: {}, Global count: {}", local_count, global_count)
+}
+
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
 
-    HttpServer::new(|| App::new()
-        .service(search)
-        .service(submit)
-        .route("/", web::get().to(HttpResponse::Ok)))
-        .bind(("127.0.0.1", 8080))?
+    let data = AppState {
+        local_count : Cell::new(0),
+        global_count : Arc::new(AtomicUsize::new(0)),
+    };
+    
+    HttpServer::new(move || {
+        App::new()
+        .app_data(web::Data::new(data.clone()))
+        .service(show_count)
+        .service(add_one)
+    }) .bind(("127.0.0.1", 8080))?
         .run()
-        .await
+        .await 
 }
