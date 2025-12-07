16ee007f1ac0e27e269024e8baa4a71d0bc8484f
maniram <maniramyadav786@gmail.com>
2025-12-07 10:23:00 +0000

    get post api added

--- actix_web_demo/src/main.rs
+++ actix_web_demo/src/main.rs
@@ -1,48 +1,58 @@
-use actix_web::{get,web, App, Responder , HttpResponse, HttpServer,Result,HttpRequest};
+use actix_web::{post,get,web, App, Responder , HttpResponse, HttpServer,Result,HttpRequest};
+use serde::Deserialize;
+use std::{
+    cell:Cell,
+    sync::{
+        Arc,
+        atomic::{AtomicUsize, Ordering},
+    },
+    
+}
 
+#[derive(Deserialize)]
 struct Info {
-    username: String,
-    age: u8,
+    username: String   ,
 }
 
-// #[get("/users/{username}/{age}")]
-// async fn index(path : web::Path<(String,String)>, json : web::Json<Info>) -> impl Responder  {
-//     let path = path.into_inner();
-//     format!("{} {} {} {}, path.0, path.1,json.username,json.age")
-// }
 
-
-// impl1 1
-// #[get("/users/{user_id}/{friend}")] // <- define path parameters
-// async fn index(path: web::Path<(u32, String)>) -> Result<String> {
-//     let (user_id, friend) = path.into_inner();
-//     Ok(format!("Welcome {}, user_id {}!", friend, user_id))
-// }
-
-
-// implindex 2
-#[get("/users/{user_id}/{friend}")] // <- define path parameters
-async fn index2(req : HttpRequest) -> Result<String> {
-    let user_id : u32 = req.match_info().get("user_id").unwrap().parse().unwrap();
-    let friend:String = req.match_info().get("friend").unwrap().to_string();
-
-    Ok(format!("Welcome {}, user_id {}!", friend, user_id))
+// Query
+#[get("/search")]
+async fn search(info : web::Query<Info>) -> String {
+    format!("Welcom: {}!", info.username)
 }
+
+
+// url encoded form data
+#[post("/submit")]
+async fn submit_form(info : web::Form<Info>) -> String {
+    format!("Welcom: {}!", info.username)
+}
+
+
+
+// Json
+#[post("/submit")]
+async fn submit(info : web::Json<Info>) -> String {
+    format!("Welcome: {}!", info.username)
+}
+
+
+// app state
+#[derive(Clone)]
+struct AppState {
+    local_count : Cell<usize>,
+    global_count : Arc<AtomicUsize>,
+}
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
 
     HttpServer::new(|| App::new()
-        // .service(index)
-        .service(index2)
+        .service(search)
+        .service(submit)
         .route("/", web::get().to(HttpResponse::Ok)))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
-
-    // multi threaded server
-    // let _= HttpServer::new(||App::new().route("/".web.get()
-    //         .to(HttpResponse::Ok))).workers(2);
-
-
 }
