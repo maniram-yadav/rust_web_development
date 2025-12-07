536a2943a257773fbc346ec47eb513c09029a837
maniram <maniramyadav786@gmail.com>
2025-12-07 10:57:26 +0000

    streaming added

---
+++ actix_web_demo/src/stream.rs
@@ -0,0 +1,24 @@
+use actix_web::{get, web, App, Error, HttpResponse, HttpServer};
+use futures::{future::ok, stream::once};
+
+
+#[get("/stream")]    
+async fn stream() ->HttpResponse{
+    let body  = once(ok::<_,Error>(web::Bytes::from_static(b"test ")));
+    HttpResponse::Ok() 
+        .content_type("text/plain")
+        .streaming(body)
+}    
+  
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(move || {
+        App::new()
+        .service(stream)
+        })
+     .bind(("127.0.0.1", 8080))?
+        .run()
+        .await 
+}
